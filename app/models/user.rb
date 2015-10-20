b31dde279865a51e55f5c4787bb576ea90378c0e
Kenneth Liu <ksliu25@gmail.com>
2015-10-20 15:53:31 -0500

    add user validations, associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+
+	has_many :subscriptions
+	has_many :channels, through: :subscriptions
+
+	validate :password_requirements
+	validates_presence_of :email, :hashed_password
+
+	def password
+		@password ||= BCrypt::Password.new(hashed_password)
+	end
+
+	def password=(new_password)
+		@raw_password = new_password
+		@password = BCrypt::Password.create(new_password)
+		self.hashed_password = @password
+	end
+
+	def self.authenticate(email, password)
+		@user = User.find_by_email(email)
+		@user if @user && @user.password == password
+	end
+
+	private
+
+	def raw_password
+		@raw_password
+	end
+
+	def password_requirements
+		if raw_password || new_record?
+			if raw_password.length < 6 || !(raw_password =~ /[!@#$%^&*]/)
+				errors.add(:password, "Password must be at least 6 characters long and contain a special character (!@#$%^&*)!")
+			end
+		end
 end
